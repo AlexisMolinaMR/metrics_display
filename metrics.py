992f3669170b649c92a451dba504029796002756
AlexisMolinaMR <alexis.molina@alumni.esci.upf.edu>
2023-01-31 17:59:52 +0100

    first improvements

--- metrics.py
+++ metrics.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 from sklearn.metrics import roc_auc_score, roc_curve, auc, confusion_matrix, classification_report
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_data(file):
     df = pd.read_csv(file)
     return df
@@ -16,15 +17,28 @@ def plot_roc_curve(y_true, y_pred, label):
     plt.legend(loc="lower right")
     return roc_auc
 
+@st.cache(allow_output_mutation=True)
+def interactive_scatter_plot(df):
+    df['error'] = abs(df['predicted'] - df['actual'])
+    fig = px.scatter(df, x='predicted', y='actual', color='error', color_continuous_scale='Viridis')
+    fig.update_layout(title='Predicted vs Actual (Colored by Absolute Error)', xaxis_title='Predicted', yaxis_title='Actual')
+    
+    return fig
+
 def main():
     st.set_option('deprecation.showPyplotGlobalUse', False)
     st.title("ROC Curve and AUC Analysis")
     file = st.file_uploader("Upload your csv file", type=["csv"])
-    threshold = st.slider("Threshold",-13.,10., -5.5, 0.1)
+    
     if file is not None:
         df = load_data(file)
         y_true = df["actual"].values
         y_pred = df["predicted"].values
+
+        fig = interactive_scatter_plot(df)
+        st.plotly_chart(fig)
+
+        threshold = st.slider("Threshold",-13.,10., -5.5, 0.1)
 
         y_pred_bin = [1 if p >= threshold else 0 for p in y_pred]
         y_true_bin = [1 if p >= threshold else 0 for p in y_true]
