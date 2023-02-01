6f308d17ad1e60f3cbe083add694acf2098922df
AlexisMolinaMR <alexis.molina@alumni.esci.upf.edu>
2023-02-01 12:56:42 +0100

    Fix 8 on fancy scatter plot - Adding trace

--- metrics.py
+++ metrics.py
@@ -21,8 +21,9 @@ def interactive_scatter_plot(df):
     def update_r2_text(selected_points_src):
         r2 = compute_r2(df, selected_points_src)
         fig.update_layout(annotations=[dict(text=f"R2: {r2:.2f}", showarrow=False, xref='paper', yref='paper', x=0.05, y=0.95, xanchor='left', yanchor='top')])
-    
-    fig.update_layout(updatemenus=[dict(type='buttons', showactive=False, buttons=[dict(label='Compute R2', method='update', args=[{'selectedPoints': fig.selectedpoints}, update_r2_text])])])
+        
+    trace = fig.data[0]
+    fig.update_layout(updatemenus=[dict(type='buttons', showactive=False, buttons=[dict(label='Compute R2', method='update', args=[{'selectedpoints': trace.selectedpoints}, update_r2_text])])])
 
     # Add R² computation
     fig.update_traces(mode='markers',
